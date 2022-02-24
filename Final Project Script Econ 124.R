b225fef7f45a48608e34a3b79a32da0acd6271ed
bigfinanceguy <83638119+bigfinanceguy@users.noreply.github.com>
2022-02-23 16:07:13 -0800

    Added proposed dataset and code for testing out a cross-validation method

---
+++ Final Project Script Econ 124.R
@@ -0,0 +1,77 @@
+rm(list = ls())
+set.seed(0)
+hospitalization <- read.csv("dataset.csv")
+attach(hospitalization)
+
+# Checking dimensions
+dim(hospitalization)
+
+Y <- hospitalization$hospital_death
+X <- subset(hospitalization, select = -c(hospital_death))
+length(Y)
+dim(X)
+Not_unique = NULL
+
+for(i in 1:ncol(hospitalization)) {      
+  if (length(unique(hospitalization[,i]))>0){
+    Column_name <- (colnames(hospitalization[i]))
+    Number_unique <- (length(unique(hospitalization[,i])))
+    Is_a_Factor <- is.factor(hospitalization[,i])
+    Not_unique = rbind(Not_unique, data.frame(Column_name,Number_unique,Is_a_Factor))}
+}
+Not_unique
+X <- subset(hospitalization, select = c(age, bmi, elective_surgery, ethnicity, gender, height, icu_admit_source, weight, heart_rate_apache, ventilated_apache, aids, cirrhosis, diabetes_mellitus, hepatic_failure, immunosuppression, leukemia, lymphoma, solid_tumor_with_metastasis))
+dim(X)
+
+Not_unique2 = NULL
+dim(X)
+for(i in 1:ncol(X)) {      
+  if (length(unique(X[,i]))<75){
+    Column_name2 <- (colnames(X[i]))
+    Number_unique2 <- (length(unique(X[,i])))
+    Is_a_Factor2 <- is.factor(X[,i])
+    Not_unique2 = rbind(Not_unique2, data.frame(Column_name2,Number_unique2,Is_a_Factor2))}
+}
+Not_unique2
+
+
+column_excluded <- c(Not_unique2$Column_name2)
+
+for(i in column_excluded){
+  print(i)
+  j <- (match(i,colnames(X)))
+  X[,j] <- as.factor(X[,j])
+}
+
+Not_unique3 = NULL
+
+for(i in 1:ncol(X)) {      
+  if (length(unique(X[,i]))<75){
+    Column_name3 <- (colnames(X[i]))
+    Number_unique3 <- (length(unique(X[,i])))
+    Is_a_Factor3 <- is.factor(X[,i])
+    Not_unique3 = rbind(Not_unique3, data.frame(Column_name3,Number_unique3,Is_a_Factor3))}
+}
+Not_unique3
+dim(X)
+
+# Removing the previous dataframes that we will no longer us
+rm(Not_unique)
+rm(Not_unique2)
+rm(Not_unique3)
+
+X <- naref(X)
+dim(X)
+# Remove Y variable from data frame that will become the X matrix
+X <- subset(X, select = -c(hospital_death))
+dim(X)
+# Making the X matrix
+#X <- model.matrix(~ ., data=X)[,-1]
+dim(X)
+# Check dimensions of our X matrix
+dim(X)
+
+
+# Estimating a logit model with lasso regularization and penalty obtained from 10-fold cross-validation
+cross_validation_NY <- cv.gamlr(X, Y, nfold = 10, family="binomial", verb=TRUE)
+
