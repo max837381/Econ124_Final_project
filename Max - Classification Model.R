ecb3ec3540d191bdc610ad0edb290c78ca9b1ffa
bigfinanceguy <83638119+bigfinanceguy@users.noreply.github.com>
2022-03-16 19:48:16 -0700

    Changed Y variable to college, added a new dataset from CPS with more variables

--- Max - Classification Model.R
+++ Max - Classification Model.R
@@ -26,20 +26,127 @@ library(caret)
 ###################################################################################################
 
 # Importing the data from Tamar's R file
-#rm(list = ls())
+rm(list = ls())
 #source("Tamar - Descriptive Statistics.R")
+
+df <- read.csv("cps_00007.csv")
+#df$unemp <- ifelse(df$EMPSTAT>=20 & df$EMPSTAT<=22,1,0)
+df$college <- ifelse(df$EDUC>=111,1,0)
+
+df$unemp <- factor(df$unemp)
+unemployed <- as.data.frame(df[df$unemp==1])
+unemployed <- df[df$unemp==1,]
+
+college <- as.data.frame(df[df$college==1])
+college <- df[df$college==1,]
+# Take out unnecessary variables
+df <- subset(df, select = -c(SERIAL, HWTFINL, CPSID, PERNUM, WTFINL, CPSIDP, VETSTAT, BPL, COVIDLOOK, COVIDTELEW, COVIDUNAW, COVIDPAID, DIFFANY, EDUC))
+
+is.factor(df$SEX)
+factor_columns <- colnames(df[colnames(df)!="AGE"])
+# Make the appropriate variables into factors
+for(i in factor_columns) {
+  df[,i] <- factor(df[,i])}
+
+is.factor(df$SEX)
+
+summary(df$RACE)
+
+## Drop race variables with very low numbers (predict function will not work if training and test sets have different number of levels)
+df<-df[!(df$RACE=="807" | df$RACE=="808" | df$RACE=="814" | df$RACE=="815" | df$RACE=="816" | df$RACE=="817" | df$RACE=="818" | df$RACE=="820"),]
+# Splitting the data into december and january
+dec21 <- df[df$YEAR==2021,]
+jan22 <- df[df$YEAR==2022,]
+
+# See if number of observations are fairly equal
+
+
 # Set.seed function to replicate the random selections from our project's R code
 set.seed(124) 
 
+# Take out year and month
+dec21 <- subset(dec21, select = -c(YEAR, MONTH))
+jan22 <- subset(jan22, select = -c(YEAR, MONTH))
+
+# Find NA values
+sum(is.na(dec21))
+mean(is.na(dec21))
+#dec21 <- dec21[, sapply(dec21, nlevels) > 1]
+
 # Split our data using 80/20 for the training set and test set
-split = sort(sample(nrow(df), nrow(df)*.8))
-train <- df[split,]
-test <- df[-split,]
+split = sort(sample(nrow(dec21), nrow(dec21)*.8))
+train <- dec21[split,]
+test <- dec21[-split,]
 
 # Run a logistic regression using training set
-logmodel <- glm(unemp ~ ., data=train, family = "binomial")
+logmodel <- glm(college ~ ., data=train, family = "binomial")
 summary(logmodel)
 ##################
+
+
+
+## Evaluate our logit model
+pred_logit1 <- drop(predict(logmodel, newdata = test, type="response"))
+
+# Plotting the histogram of predicted probabilities
+hist(pred_logit1, xlab = "Predicted Probabilities")
+
+Y <- train$college
+X <- train[colnames(train)!="college"]
+
+library(gamlr)
+
+
+# Making the X matrix
+X <- sparse.model.matrix(~ ., data=X)[,-1]
+dim(X)
+# Estimating a logit model with lasso regularization and penalty obtained from 10-fold cross-validation
+cross_validation <- cv.gamlr(X, Y, nfold = 10, family="binomial", verb=TRUE)
+
+
+plot(cross_validation$gamlr) # lasso regularization path
+plot(cross_validation) # plot of cross-validation error against lambda
+
+# Number of non-zero coefficients in the optimal CV model minus the intercept
+cat("number of nonzero coefficients for CV-optimal lambda:", length(which(coef(cross_validation, select="min")!=0))-1, "\n")
+
+# Number of total coefficients of our optimal CV model
+cat("number of total coefficients for CV-optimal lambda:", length(coef(cross_validation, select="min"))-1, "\n")
+
+# Computing the predicted probabilities
+pred_CV <- drop(predict(cross_validation, select='min', X, type="response"))
+
+# Plotting the histogram of predicted probabilities
+hist(pred_CV, xlab = "Predicted Probabilities")
+
+# Setting seed and loading in the ROC.R
+set.seed(0)
+source("roc.R")
+
+cutoff_points = c(0.02,0.1,0.33,0.8,0.9)
+cutoff_colors = c("blue","red","green","yellow","purple")
+
+# IS curve
+par(mai=c(.9,.9,.2,.1)) # format margins
+roc(pred_CV, Y, bty="n", main="IS ROC") # from roc.R
+
+# For loop to make my code more concise
+for (i in cutoff_points){
+  points(x=1-mean((pred_CV<=i)[Y==0]), y=mean((pred_CV>i)[Y==1]), cex=1.5, pch=20, col=cutoff_colors[match(i,cutoff_points)])
+}
+
+legend("bottomright",fill=cutoff_colors, legend=c(cutoff_points),bty="n",title="cutoffs")
+
+#df[colnames(df)!="AGE"]
+
+pred_acc <- as.factor(ifelse(pred_CV>0.5,1,0))
+
+# Evaluate and show why we might want to use machine learning models
+
+# Caret library to compute a confusion matrix
+library(caret)
+confusionMatrix(data = pred_acc,
+                reference = test$college)
 
 ## This part of the code doesn't work yet
 ## \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
