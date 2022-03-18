fd7e8095d6cea436106f56c7343b6105a9b6555d
Tamar Sasson <tsasson@ucsc.edu>
2022-03-17 22:43:09 -0700

    dropped white people from the data

--- Tamar - Descriptive Statistics.R
+++ Tamar - Descriptive Statistics.R
@@ -3,17 +3,10 @@
 #regression and then scatter plot
 library(dplyr)
 library(tidyr)
+library(ggplot2)
 df <- read.csv("cps_00010.csv") #loads the dataset
 df <- df[df$AGE >= 22,] #restricts dataset to ages where one typically gets a bachelor's degree
 df$college <- ifelse(df$EDUC>=111,1,0) #creates a dummy variable =1 if person has a bachelor's degree (or higher)
-
-#gender descriptive statistics
-gender_totals <- df %>%
-  group_by(SEX) %>%
-  summarise(totals=sum(tabulate(SEX)))
-gender_college_totals <- df %>%
-  group_by(SEX, college) %>%
-  summarise(totals=sum(tabulate(SEX)))
 
 #race descriptive statistics
 #groupings for race
@@ -27,13 +20,33 @@ df$race_group[df$RACE==300] <- 3 #=3 if person is Native American
 df$race_group[df$asian_pi==1] <- 4 #=4 if person is Asian or Pacific Islander
 df$race_group[df$mixed_ancestry==1] <- 5 #=5 if person has mixed ancestry of 2 or more races
 
+df$total_white_college <- ifelse(df$race_group==1 & df$college==1,1,0) #=1 if person is white and completed college
+
+tabulate(as.numeric(df$total_white_college)) #calculates the total number of white people in college
+
+df <- df[df$race_group != 1,] #drops white people from the dataset
+
 race_totals <- df %>%
   group_by(race_group) %>%
-  summarise(totals=sum(tabulate(race_group)))
+  summarise(totals=sum(tabulate(race_group))) #data with the total number of each race group
 race_college_totals <- df %>%
   group_by(race_group, college) %>%
   summarise(totals=sum(tabulate(race_group)))
+race_college <- race_college_totals[race_college_totals$college==1,] #data with the total number that completed 
+#college of each race group
 
+ggplot(race_college, aes(y=totals, x=race_group)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  ggtitle("Total Number of Individuals Who \n Completed College by Race") +
+  xlab("Race Group") +
+  ylab("Total Number of Individuals") #plots the total number of people who completed college by race
+#gender descriptive statistics
+gender_totals <- df %>%
+  group_by(SEX) %>%
+  summarise(totals=sum(tabulate(SEX)))
+gender_college_totals <- df %>%
+  group_by(SEX, college) %>%
+  summarise(totals=sum(tabulate(SEX)))
 #income grouping
 quantile(df$FAMINC, probs = c(0.25,0.5,0.75))
 df$lowerclass <- ifelse(df$FAMINC <= 730,1,0) #classifies someone as being lower class if their family income is less than or equal to $39,999
@@ -56,7 +69,12 @@ income_totals <- df %>%
 income_college_totals <- df %>%
   group_by(income_group, college) %>%
   summarise(totals=sum(tabulate(income_group)))
+income_college <- income_college_totals[income_college_totals$college==1,]
 
 hist(as.numeric(df$incomegroup), main = "Histogram of Income Group",
      xlab="Income Group")
-
+ggplot(income_college, aes(y=totals, x=income_group)) +
+  geom_bar(position = "dodge", stat = "identity") +
+  ggtitle("Total Number of Individuals Who \n Completed College by Income Group") +
+  xlab("Income Group") +
+  ylab("Total Number of Individuals")
